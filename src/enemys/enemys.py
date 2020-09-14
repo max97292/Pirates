29da97dc38ed4961dc2e43e0b3cb0bfd5208ab39
Maksym Diachenko <max97292@gmail.com>
2020-09-15 00:43:38 +0300

    Commit #11 Add error message to search enemy Add select item to attack from kit on person Add player agility, item damage, item usability, item type Create player attack

--- src/enemys/enemys.py
+++ src/enemys/enemys.py
@@ -13,7 +13,10 @@ bot = telebot.TeleBot(TOKEN)
 conn = sqlite3.connect("DataBase.db", check_same_thread=False)
 cursor = conn.cursor()
 
+
 def select_enemys(message):
+    search = False
+
     ENEMYS = []
 
     kb_attack = types.ReplyKeyboardMarkup(True, False)
@@ -23,59 +26,69 @@ def select_enemys(message):
     try:
         cursor.execute('select * from status where id_player=?', [message.from_user.id])
         status = cursor.fetchone()
-
-        if status[9] == 'beach':
-            cursor.execute('select * from enemy')
-            enemys = cursor.fetchall()
-            for item in enemys:
-                id = str(item[0])
-                if id.startswith('2'):
-                    ENEMYS.append(item)
-
-        if status[9] == 'colony':
-            cursor.execute('select * from enemy')
-            enemys = cursor.fetchall()
-            for item in enemys:
-                id = str(item[0])
-                if id.startswith('3'):
-                    ENEMYS.append(item)
-
-        if status[9] == 'forest_big':
-            cursor.execute('select * from enemy')
-            enemys = cursor.fetchall()
-            for item in enemys:
-                id = str(item[0])
-                if id.startswith('5'):
-                    ENEMYS.append(item)
-        elif status[9] == 'forest':
-            cursor.execute('select * from enemy')
-            enemys = cursor.fetchall()
-            for item in enemys:
-                id = str(item[0])
-                if id.startswith('4'):
-                    ENEMYS.append(item)
-
-        if status[9] == 'cave':
-            cursor.execute('select * from enemy')
-            enemys = cursor.fetchall()
-            for item in enemys:
-                id = str(item[0])
-                if id.startswith('7'):
-                    ENEMYS.append(item)
     except Exception as e:
         print(e)
 
-    selected_enemy = random.randint(0, len(ENEMYS)-1)
-    selected_enemy = ENEMYS[selected_enemy]
+    if status[9] == 'beach':
+        cursor.execute('select * from enemy')
+        enemys = cursor.fetchall()
+        for item in enemys:
+            id = str(item[0])
+            if id.startswith('2'):
+                ENEMYS.append(item)
+                search = True
 
-    try:
-        cursor.execute('update enemy_status set id_enemy=? where id_player=?', [selected_enemy[0], message.from_user.id])
-        conn.commit()
-    except Exception as e:
-        print(e)
+    if status[9] == 'colony':
+        cursor.execute('select * from enemy')
+        enemys = cursor.fetchall()
+        for item in enemys:
+            id = str(item[0])
+            if id.startswith('3'):
+                ENEMYS.append(item)
+                search = True
 
+    if status[9] == 'forest_big':
+        cursor.execute('select * from enemy')
+        enemys = cursor.fetchall()
+        for item in enemys:
+            id = str(item[0])
+            if id.startswith('5'):
+                ENEMYS.append(item)
+                search = True
 
-    bot.send_message(message.chat.id, 'Ты встретил %s на своём пути' % selected_enemy[1], reply_markup=kb_attack)
+    elif status[9] == 'forest':
+        cursor.execute('select * from enemy')
+        enemys = cursor.fetchall()
+        for item in enemys:
+            id = str(item[0])
+            if id.startswith('4'):
+                ENEMYS.append(item)
+                search = True
+
+    if status[9] == 'cave':
+        cursor.execute('select * from enemy')
+        enemys = cursor.fetchall()
+        for item in enemys:
+            id = str(item[0])
+            if id.startswith('7'):
+                ENEMYS.append(item)
+                search = True
+
+    if search == False:
+        bot.send_message(message.chat.id, 'Тут произошла ошибочка')
+    else:
+        selected_enemy = random.randint(0, len(ENEMYS) - 1)
+        selected_enemy = ENEMYS[selected_enemy]
+
+        try:
+            cursor.execute('update enemy_status set id_enemy=? where id_player=?',
+                           [selected_enemy[0], message.from_user.id])
+            conn.commit()
+        except Exception as e:
+            print(e)
+
+        bot.send_message(message.chat.id, 'Ты встретил %s на своём пути' % selected_enemy[1], reply_markup=kb_attack)
+
 
 def attack_enemy(message):
     try:
@@ -85,12 +98,13 @@ def attack_enemy(message):
         status = cursor.fetchone()
         cursor.execute('select * from enemy where id=?', [enemy_status[1]])
         enemy = cursor.fetchone()
+        cursor.execute('select kit from equipment where id_player=?', [message.from_user.id])
+        kit = cursor.fetchone()
+        kit = kit[0]
+        cursor.execute('select * from items where id=?', [kit])
+        item = cursor.fetchone()
     except Exception as e:
         print(e)
-
-    print(enemy_status)
-    print(status)
-    print(enemy)
 
     player_health = status[4]
     monster_health = enemy[2]
@@ -104,24 +118,59 @@ def attack_enemy(message):
     player_protection = status[7]
     monster_protection = enemy[4]
 
+    player_agility = status[10]
+
     monster_name = enemy[1]
 
     monster_type = enemy[5]
     monster_incoming = enemy[6]
     monster_outgoing = list(str(enemy[7]))
 
+    item_damage = item[2]
+    item_usability = item[3]
+    item_usability = item_usability/100
+    item_type = item[6]
+
     message_out = 'Бой против %s \n\n' % (monster_name)
 
-    while True:
+    '''while True:
         if player_health <= 0:
             bot.send_message(message.chat.id, message_out)# + '\n❤0/%d\n') % [player_full_health[0] ]
-            break
+            break'''
 
-    step = random.randint(0,1)
+    # step = random.randint(0,1)
+    step = 0
     if step == 0:
-        dodge = random.uniform(0, 1)
-        if dodge > 0.2 and dodge < 0.6:
-            bot.send_message(message.chat.id, 'Увернулся')
+        damage = player_power
+
+        low_damage = damage * 0.5
+        high_damage = damage * 1.5
+
+        usability = random.uniform(0, 1)
+        if (item_usability) > usability:
+            low_damage = damage * item_usability
+        attack = random.randint(low_damage, high_damage)
+
+        if monster_incoming == 1:
+            if item_type == 2:
+                pass
+            else:
+                attack -= attack * random.uniform(0, 0.3)
+        if monster_incoming == 2:
+            if item_type == 1:
+                pass
+            elif item_type == 2:
+                attack -= attack * random.uniform(0, 0.1)
+            else:
+                attack -= attack * random.uniform(0, 0.9)
+
+        chance = random.uniform(0,1)
+        if chance < 0.125:
+            bot.send_message(message.chat.id, 'Уклонился')
         else:
-            player_hit = player_power - monster_protection
-            monster_health -+ player_hit
+            chance = random.uniform(0, 0.875)
+            if chance < 0.125:
+                attack *= 0.5
+                bot.send_message(message.chat.id, 'Избежал урон\nАтака: %d\nLow/high: %d/%d' % (attack, low_damage, high_damage))
+            else:
+                bot.send_message(message.chat.id, 'Атака: %d\nLow/high: %d/%d' % (attack, low_damage, high_damage))
